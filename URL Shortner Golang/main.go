79299e00557ee810ba9c284d53c5e41daf2f31df
aaditya29 <mishraaaditya29@gmail.com>
2021-07-17 01:01:54 +0530

    code commit

--- URL Shortner Golang/main.go
+++ URL Shortner Golang/main.go
@@ -1,1 +1,13 @@
 package main
+
+type Config struct { // Config is a struct describing the configuration which are parsed from command line arguments
+	PathToYAML string
+	PathToJSON string
+}
+
+func main() {
+	config := getConfig()
+
+	yamlBytes := getFileBytes(config.PathToYAML)
+	jsonBytes := getFileBytes(config.PathToJSON)
+}
