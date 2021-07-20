761dd428bf84a3ea262df9fa6ef1b2434c8d6964
aaditya29 <mishraaaditya29@gmail.com>
2021-07-21 01:20:41 +0530

    code commit

--- URL Shortner Golang/handlers.go
+++ URL Shortner Golang/handlers.go
@@ -8,4 +8,6 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	   that will attempt to map any paths (keys in the map) to their corresponding URL (values) that each key in the map points to, in string format).
 	*/
 
+	// If the path is not provided in the map, then the fallback http.Handler will be called instead.
+
 }
