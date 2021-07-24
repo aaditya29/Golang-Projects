5e4ca150ce62f6b031db17f372a6f3c11e9a3908
aaditya29 <mishraaaditya29@gmail.com>
2021-07-25 00:08:44 +0530

    code commit

--- Virology/main.go
+++ Virology/main.go
@@ -1,7 +1,10 @@
 // code to start the server
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", handler) // The "HandleFunc" method accepts a path and a function as arguments
@@ -10,5 +13,5 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) { // handler is the handler function. It has to follow the function signature of a ResponseWriter and Request type as the arguments.
-
+	fmt.Fprintf(w, "Hello World!")
 }
