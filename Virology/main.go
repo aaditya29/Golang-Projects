26051741dc1783d48e970fa310f8edc25fae92b1
aaditya29 <mishraaaditya29@gmail.com>
2021-07-25 00:04:40 +0530

    code commit

--- Virology/main.go
+++ Virology/main.go
@@ -1,2 +1,9 @@
 // code to start the server
 package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", handler) // The "HandleFunc" method accepts a path and a function as arguments
+
+}
