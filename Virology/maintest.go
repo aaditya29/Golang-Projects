7a979b5dae1f0690984d1db633c9d14bab204b5c
aaditya29 <mishraaaditya29@gmail.com>
2021-07-26 00:21:53 +0530

    code commit

--- Virology/maintest.go
+++ Virology/maintest.go
@@ -1,8 +1,13 @@
 //testing handler
 package main
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
 func TestHandler(t *testing.T) { // In this function, forming a new HTTP request which is going to be passed to our handler.
 
+	req, err := http.NewRequest("GET", "", nil) // here first argument is the method, the second argument is the route (which we are leaving blank for a while)
+	//and the third is the request body, which we don't have in this case.
 }
