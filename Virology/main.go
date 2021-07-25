5a1f3b6b7ceb6b42295d82168151807b3b0a1b4f
aaditya29 <mishraaaditya29@gmail.com>
2021-07-26 00:09:15 +0530

    code commit

--- Virology/main.go
+++ Virology/main.go
@@ -4,12 +4,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	r := mux.NewRouter() //Declaring a new router
+
+	r.HandleFunc("/hello", handler).Methods("GET") //This allows us to declare METHODS this path will be valid for
+
 	http.HandleFunc("/", handler) // The "HandleFunc" method accepts a path and a function as arguments
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) { // handler is the handler function. It has to follow the function signature of a ResponseWriter and Request type as the arguments.
