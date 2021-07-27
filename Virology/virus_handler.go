6945ec3eb68e9192224f3f4af852d8fac5ff81ee
aaditya29 <mishraaaditya29@gmail.com>
2021-07-27 11:37:06 +0530

    code commit

--- Virology/virus_handler.go
+++ Virology/virus_handler.go
@@ -8,5 +8,12 @@ var virus []Virus
 
 //Defining handler to fetch the details of all the viruses
 func getVirusHandler(w http.ResponseWriter, r *http.Request) {
+	//Converting the virus variable to json
+	virusListBytes, err := json.Marshal(virus)
 
+	if err != nil { //if error occurs
+		fmt.Println(fmt.Errorf("Error: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
